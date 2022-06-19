ce3621a38c4f1cbcf09ffaea408a7be37086acbe
moffy-Black <black.river.emperor@outlook.jp>
2022-06-19 13:57:47 +0900

    [修正]portの設定

--- app/main.py
+++ app/main.py
@@ -97,4 +97,4 @@ async def return_edited_img(
 
 
 if __name__ == "__main__":
-    uvicorn.run(host="127.0.0.1", port=55580, reload=True, log_level="info")
+    uvicorn.run(host="127.0.0.1", port=80, reload=True, log_level="info")
